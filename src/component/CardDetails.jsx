a531c218c6f8ab23415779f424400e619686a0b2
sauravsamui <samuisaurav1@gmail.com>
2022-05-20 21:54:30 +0530

    payment-card

---
+++ src/component/CardDetails.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import style from "./Cardstyle.module.css"
+const CardDetails = ({details}) => {
+    console.log(details)
+  return (
+    <div className={style.big_div} style={{backgroundColor:details.heading=="Amazon Gift"?"orange":"grey"}}>
+        <div className={style.logo}>
+            <h2>{details.date}</h2>
+            <img className={style.img} src={details.logo} alt="" />
+        </div>
+        <h3 className={style.case}>{details.case}</h3>
+        <h1 style={{fontSize:"50px"}}>{details.heading}</h1>
+        <h1 style={{fontSize:"50px"}}>{details.sub_heading}</h1>
+        <div className={style.logo}>
+            <h2>{details.device}</h2>
+            <h1 style={{fontSize:"35px"}}>→</h1>
+        </div>
+    </div>
+  )
+}
+
+export default CardDetails
